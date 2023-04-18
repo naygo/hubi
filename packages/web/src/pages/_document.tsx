f28c4e175897093801bb1c03f5966bdafee8caa6
Ives M. Watanabe <ivesmw@gmail.com>
2023-04-17 21:52:27 -0300

    feat: add new fonts

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -1,9 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anton&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Icon */}
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
@@ -11,5 +24,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
