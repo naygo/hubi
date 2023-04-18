f93b85a003e597e4b3b86fb443ad4a7fa0b97651
Nayla Gomes <nayla.cgs@gmail.com>
2023-04-18 00:17:36 -0300

    feat: add footer and background images

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -3,16 +3,23 @@ import { Button } from '@/shared/components/Button'
 import Image from 'next/image'
 import styles from './styles.module.css'
 
-import logo from '../shared/assets/img/header.png'
+import header from '../shared/assets/img/header.png'
+import logo from '../shared/assets/img/LogoDarkBackground.svg'
+import logoTwitter from '../shared/assets/img/LogoTwitter.svg'
+import logoDiscord from '../shared/assets/img/LogoDiscord.svg'
+import logoNouns from '../shared/assets/img/LogoNouns.svg'
 
 export default function Home() {
   return (
-    <div>
+    <div className='h-full'>
       <header className={styles.header}>
-        <Image src={logo} alt='HUB Inclusivo'className={styles.headerImage}/>
+        <Image src={header} alt='HUB Inclusivo' className={styles.headerImage} />
       </header>
 
-      <div className="flex flex-col justify-center items-center mt-16">
+      <div className={styles.leftBackground}></div>
+      <div className={styles.rightBackground}></div>
+
+      <div className="flex flex-col justify-center items-center mt-16 my-16">
         <div className="text-center max-w-lg">
           <h1 className={styles.title}>OLÁ!</h1>
           <p className="mt-4">
@@ -35,11 +42,19 @@ export default function Home() {
         </div>
 
         <div className='mt-8'>
-        <Button label="Saiba mais" outline/>
+          <Button label="Saiba mais" outline />
         </div>
-        
       </div>
-      <footer></footer>
+
+      <footer className='flex justify-between px-10'>
+        <Image src={logo} alt='HUB Inclusivo' className={styles.footerImage} />
+
+        <div className='h-full flex justify-center align-bottom gap-4'>
+          <Image src={logoTwitter} alt='HUB Inclusivo' className={`h-5`} />
+          <Image src={logoDiscord} alt='HUB Inclusivo' className={`h-5`} />
+          {/* <Image src={logoNouns} alt='HUB Inclusivo' className={styles.footerImage} /> */}
+        </div>
+      </footer>
     </div>
   )
 }
