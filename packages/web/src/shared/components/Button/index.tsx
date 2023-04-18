bed0b0f46e089ada72fb13621979dfe9d3eddaf9
Nayla Gomes <nayla.cgs@gmail.com>
2023-04-17 21:16:39 -0300

    feat: outline button and default button

--- packages/web/src/shared/components/Button/index.tsx
+++ packages/web/src/shared/components/Button/index.tsx
@@ -1,21 +1,27 @@
+import styles from "./styles.module.css";
+
 interface ButtonProps {
   label: string;
   outline?: boolean;
 }
 
-import styles from "./styles.module.css";
-
 export const Button = ({ label, outline = false }: ButtonProps) => {
   return (
     <main>
-    <button className={`
-      ${outline ? styles.outlineButton : styles.defaultButton}
-      rounded-full
-    `}>
-      {label}
-    </button>
+      <button
+        className={`
+          ${outline ? styles.outlineButton : styles.defaultButton}
+          rounded-full 
+          py-2
+          px-4 
+          font-semibold 
+          text-lg 
+          transition
+          duration-200
+        `}
+      >
+        {label.toUpperCase()}
+      </button>
     </main>
-
-    
   );
 };
