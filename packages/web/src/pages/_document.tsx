1ac128ef15e3c50900e0824214c68603d7a2b061
Nayla Gomes <nayla.cgs@gmail.com>
2023-04-18 21:11:33 -0300

    feat: config fonts

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -15,6 +15,18 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Anton&family=Barlow+Semi+Condensed&family=Oswald&family=Tajawal&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Antonio:wght@700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
 
         {/* Icon */}
         <link rel="shortcut icon" href="/favicon.ico" />
