7b008568173512e10e0a464dcb3f2d0518111671
Ives M. Watanabe <ivesmw@gmail.com>
2023-04-25 21:15:35 -0300

    feat: add GetLeaderboardService

---
+++ packages/api/src/domain/services/faceit/GetLeaderboard.service.ts
@@ -0,0 +1,19 @@
+import { ok, serverError } from '@/domain/helpers'
+import { HttpResponse, Service } from '@/domain/interfaces/protocols'
+import { FaceitApiClient } from '@/infra/integrations/interfaces'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class GetLeaderboardService implements Service {
+  constructor(private readonly faceitApiClient: FaceitApiClient) {}
+
+  async execute(): Promise<HttpResponse> {
+    try {
+      const leaderboard = await this.faceitApiClient.getLeaderboard()
+
+      return ok(leaderboard)
+    } catch (err) {
+      return serverError()
+    }
+  }
+}
