5d3a0fba081bd16dfe50d87d2a354e251b7d685d
Nayla Gomes <nayla.cgs@gmail.com>
2023-04-25 23:32:55 -0300

    feat: add background images on leaderboard page

--- packages/web/src/pages/leaderboard/index.tsx
+++ packages/web/src/pages/leaderboard/index.tsx
@@ -8,45 +8,50 @@ interface LeaderboardProps {
 
 export default function Leaderboard({ leaderboard }: LeaderboardProps) {
   return (
-    <div className="flex justify-center h-full items-center">
-      <div className="flex flex-col items-center w-full lg:max-w-5xl">
-        <div>
-          <h1 className={`text-6xl sm:text-8xl`}>LEADERBOARD</h1>
-        </div>
-        <input
-          className={`${styles.input} my-5 sm:m-10 w-10/12 sm:max-w-lg`}
-          name="player"
-          placeholder="Pesquisar uma jogadora..."
-        />
-        <div className={`${styles.table} w-full p-3 overflow-auto`}>
-          <div className="grid grid-cols-12 text-center">
-            <p className="col-span-3 md:col-span-2 text-sm sm:text-xl">
-              CLASSIFICAÇÃO
-            </p>
-            <p className="col-span-3 md:col-span-3 text-sm  sm:text-xl">
-              PONTUAÇÃO
-            </p>
-            <p className="col-span-6 md:col-span-4 text-sm sm:text-xl"></p>
-            <p className="md:col-span-3 text-xs hidden md:block sm:text-xl">
-              PARTIDAS
-            </p>
+    <div className="h-full">
+      <div className={styles.leftBackground}></div>
+      <div className={styles.rightBackground}></div>
+
+      <div className="flex justify-center h-full items-center">
+        <div className="flex flex-col items-center w-full lg:max-w-5xl">
+          <div>
+            <h1 className={`text-6xl sm:text-8xl`}>LEADERBOARD</h1>
           </div>
-          {leaderboard.map((player, index) => (
-            <div
-              w-full
-              key={player.userId}
-              className={`${styles.tableContent} ${
-                index == 0 ? 'h-20 rounded-b-3xl' : 'h-10 rounded-full'
-              } mt-3 text-center grid grid-cols-12 gap-6 items-center`}
-            >
-              <p className="col-span-3 md:col-span-2">{index + 1}</p>
-              <p className="col-span-3 md:col-span-3">{player.points}</p>
-              <p className="col-span-6 md:col-span-4 ">{player.nickname}</p>
-              <p className="md:col-span-3 hidden md:block">
-                {player.played} {player.played == 1 ? 'Partida' : 'Partidas'}{' '}
+          <input
+            className={`${styles.input} my-5 sm:m-10 w-10/12 sm:max-w-lg`}
+            name="player"
+            placeholder="Pesquisar uma jogadora..."
+          />
+          <div className={`${styles.table} w-full p-3 overflow-auto`}>
+            <div className="grid grid-cols-12 text-center">
+              <p className="col-span-3 md:col-span-2 text-sm sm:text-xl">
+                CLASSIFICAÇÃO
+              </p>
+              <p className="col-span-3 md:col-span-3 text-sm  sm:text-xl">
+                PONTUAÇÃO
+              </p>
+              <p className="col-span-6 md:col-span-4 text-sm sm:text-xl"></p>
+              <p className="md:col-span-3 text-xs hidden md:block sm:text-xl">
+                PARTIDAS
               </p>
             </div>
-          ))}
+            {leaderboard.map((player, index) => (
+              <div
+                w-full
+                key={player.userId}
+                className={`${styles.tableContent} ${
+                  index == 0 ? 'h-20 rounded-b-3xl' : 'h-10 rounded-full'
+                } mt-3 text-center grid grid-cols-12 gap-6 items-center`}
+              >
+                <p className="col-span-3 md:col-span-2">{index + 1}</p>
+                <p className="col-span-3 md:col-span-3">{player.points}</p>
+                <p className="col-span-6 md:col-span-4 ">{player.nickname}</p>
+                <p className="md:col-span-3 hidden md:block">
+                  {player.played} {player.played == 1 ? 'Partida' : 'Partidas'}{' '}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
