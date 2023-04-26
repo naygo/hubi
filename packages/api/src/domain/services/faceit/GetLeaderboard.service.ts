0b20e9eadbdaab45079efff27849b730f6f3793a
Ives M. Watanabe <ivesmw@gmail.com>
2023-04-26 14:33:23 -0300

    feat: format leaderboard

--- packages/api/src/domain/services/faceit/GetLeaderboard.service.ts
+++ packages/api/src/domain/services/faceit/GetLeaderboard.service.ts
@@ -1,6 +1,6 @@
 import { ok, serverError } from '@/domain/helpers'
 import { HttpResponse, Service } from '@/domain/interfaces/protocols'
-import { FaceitApiClient } from '@/infra/integrations/implementations'
+import { FaceitApiClient } from '@/infra/integrations/implementations/faceitApi'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
@@ -11,9 +11,24 @@ export class GetLeaderboardService implements Service {
     try {
       const leaderboard = await this.faceitApiClient.getLeaderboard()
 
-      return ok(leaderboard)
+      const formattedLeaderboard: GetLeaderboardResponse[] =
+        leaderboard.items.map((item) => ({
+          userId: item.player.user_id,
+          nickname: item.player.nickname,
+          played: item.played,
+          points: item.points,
+        }))
+
+      return ok({ leaderboard: formattedLeaderboard })
     } catch (err) {
       return serverError()
     }
   }
+}
+
+export interface GetLeaderboardResponse {
+  userId: string
+  nickname: string
+  played: number
+  points: number
 }
