658992305682a4102224a8e86ae9e1796456dc2e
Vinicius Marinho <viniciusalexsander1@gmail.com>
2023-04-27 22:46:56 -0300

    feat: Busca por player

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 
 import { AppModule } from './app.module'
+import cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.useGlobalPipes(new ValidationPipe())
+
+  const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  }
+  app.use(cors(corsOptions))
 
   await app.listen(3030)
 }
--- packages/web/src/pages/leaderboard/index.tsx
+++ packages/web/src/pages/leaderboard/index.tsx
@@ -1,15 +1,43 @@
 import { GetStaticProps } from 'next'
 import { BsFillArrowLeftCircleFill } from 'react-icons/bs'
 
-import styles from './styles.module.scss'
 import { Leaderboard, getLeaderboard } from '@/services/leaderboard'
+import { IGetPlayerResponse, getPlayer } from '@/services/player'
+import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import seasons from '../../shared/assets/img/seasons.svg'
+import styles from './styles.module.scss'
 
 interface LeaderboardProps {
   leaderboard: Leaderboard[]
 }
 
 export default function Leaderboard({ leaderboard }: LeaderboardProps) {
+  const [player, setPlayer] = useState<IGetPlayerResponse | null>(null)
+
+  const handleSearch = async (nickname: string) => {
+    if (nickname && nickname.length > 3) {
+      try {
+        const playerResult = await getPlayer({ nickname })
+        setPlayer(playerResult)
+      } catch (error) {
+        setPlayer(null)
+        toast.info('Não foi encontrado um player com o nickname informado!', {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+        })
+      }
+    } else setPlayer(null)
+  }
+
   return (
     <div className="h-full">
       <Link className={`absolute ${styles.arrowBack}`} href="/">
@@ -20,15 +48,17 @@ export default function Leaderboard({ leaderboard }: LeaderboardProps) {
 
       <div className="flex justify-center h-full items-center">
         <div className="flex flex-col items-center w-full lg:max-w-5xl">
-          <div>
-            <h1 className={`text-6xl sm:text-8xl`}>LEADERBOARD</h1>
-          </div>
+          <h1 className={`text-6xl sm:text-8xl`}>LEADERBOARD</h1>
 
-          <input
-            className={`${styles.input} my-5 sm:m-10 w-10/12 sm:max-w-lg focus:outline-none`}
-            name="player"
-            placeholder="Pesquisar uma jogadora..."
-          />
+          <div className="flex flex-row w-full justify-center">
+            <input
+              className={`${styles.input} my-5 sm:m-10 w-10/12 sm:max-w-lg focus:outline-none`}
+              name="player"
+              placeholder="Pesquisar uma jogadora..."
+              onChange={(event) => handleSearch(event.target.value)}
+            />
+            <Image src={seasons} alt="Season atual" />
+          </div>
 
           <div className={`${styles.table} w-full p-3 overflow-auto`}>
             <div className="grid grid-cols-12 text-center">
@@ -43,24 +73,41 @@ export default function Leaderboard({ leaderboard }: LeaderboardProps) {
                 PARTIDAS
               </p>
             </div>
-            {leaderboard.map((player, index) => (
+            {player ? (
               <div
                 w-full
                 key={player.userId}
-                className={`${styles.tableContent} ${
-                  index == 0
-                    ? `h-20 rounded-b-3xl ${styles.tableFirstRow}`
-                    : 'h-10 rounded-full'
-                } mt-3 text-center grid grid-cols-12 gap-6 items-center`}
+                className={`${styles.tableContent} h-20 rounded-b-3xl ${styles.tableFirstRow} mt-3 text-center grid grid-cols-12 gap-6 items-center`}
               >
-                <p className="col-span-3 md:col-span-2">{index + 1}</p>
+                <p className="col-span-3 md:col-span-2"></p>
                 <p className="col-span-3 md:col-span-3">{player.points}</p>
                 <p className="col-span-6 md:col-span-4 ">{player.nickname}</p>
                 <p className="md:col-span-3 hidden md:block">
                   {player.played} {player.played == 1 ? 'Partida' : 'Partidas'}{' '}
                 </p>
               </div>
-            ))}
+            ) : (
+              leaderboard.length > 0 &&
+              leaderboard.map((player, index) => (
+                <div
+                  w-full
+                  key={player.userId}
+                  className={`${styles.tableContent} ${
+                    index == 0
+                      ? `h-20 rounded-b-3xl ${styles.tableFirstRow}`
+                      : 'h-10 rounded-full'
+                  } mt-3 text-center grid grid-cols-12 gap-6 items-center`}
+                >
+                  <p className="col-span-3 md:col-span-2">{index + 1}</p>
+                  <p className="col-span-3 md:col-span-3">{player.points}</p>
+                  <p className="col-span-6 md:col-span-4 ">{player.nickname}</p>
+                  <p className="md:col-span-3 hidden md:block">
+                    {player.played}{' '}
+                    {player.played == 1 ? 'Partida' : 'Partidas'}{' '}
+                  </p>
+                </div>
+              ))
+            )}
           </div>
         </div>
       </div>
@@ -70,60 +117,9 @@ export default function Leaderboard({ leaderboard }: LeaderboardProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
   const leaderboard = await getLeaderboard()
-  console.log(leaderboard)
+
   return {
     props: { leaderboard },
     revalidate: 60 * 60 * 24, // 24hours
   }
 }
-
-const rank = [
-  {
-    id: 1,
-    pontuacao: 1213,
-    nickname: 'azureesz',
-    partidas: 1,
-  },
-  {
-    id: 2,
-    pontuacao: 234,
-    nickname: 'lyssaz',
-    partidas: 1,
-  },
-  {
-    id: 3,
-    pontuacao: 1213,
-    nickname: 'ilorde',
-    partidas: 1,
-  },
-  {
-    id: 4,
-    pontuacao: 1213,
-    nickname: 'erinvlr',
-    partidas: 1,
-  },
-  {
-    id: 5,
-    pontuacao: 1213,
-    nickname: 'aurayse',
-    partidas: 1,
-  },
-  {
-    id: 6,
-    pontuacao: 1213,
-    nickname: 'Rapunzel_f',
-    partidas: 1,
-  },
-  {
-    id: 7,
-    pontuacao: 1213,
-    nickname: 'patomac',
-    partidas: 1,
-  },
-  {
-    id: 8,
-    pontuacao: 1213,
-    nickname: '1srN',
-    partidas: 1,
-  },
-]
