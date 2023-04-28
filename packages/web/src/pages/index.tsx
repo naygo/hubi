df465e6e879a36bfac269cb1bc3f88b9b435c6b2
Nayla Gomes <nayla.cgs@gmail.com>
2023-04-28 15:19:19 -0300

    feat: add title in pages

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/shared/components/Button'
 // import logoNouns from '../shared/assets/img/LogoNouns.svg'
 
 import styles from './styles.module.scss'
+import Head from 'next/head'
 
 function openLinkTree() {
   window.open('https://linktr.ee/hub.inclusivo', '_blank')
@@ -11,42 +12,52 @@ function openLinkTree() {
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between">
-      <div
-        className={`flex flex-col justify-center items-center mt-16 gap-y-6 p-3`}
-      >
-        <h1 className={`text-8xl yellow`}>OLÁ!</h1>
-
+    <>
+      <Head>
+        <title>HUBI</title>
+      </Head>
+      <div className="flex flex-col justify-between">
         <div
-          className={`text-center my-8 flex flex-col gap-y-4 ${styles.presentationText}`}
-          style={{ maxWidth: '40rem' }}
+          className={`flex flex-col justify-center items-center mt-16 gap-y-6 p-3`}
         >
-          <p>
-            Bem-vindo(a) a <b>HUB Inclusivo</b> de <b>VALORANT!</b>
-          </p>
-          <p>
-            Somos uma comunidade dedicada a promover a competitividade e
-            evolução das jogadoras do cenário inclusivo de VALORANT.
-          </p>
-          <p>
-            Acreditamos na importância de proporcionar um ambiente amigável onde
-            as mulheres cis, trans e pessoas não-binárias possam se sentir
-            representadas e respeitadas.
-          </p>
-          <p>
-            A missão da HUB é garantir um ambiente altamente competitivo,
-            melhorando a descoberta, o desenvolvimento e a promoção de novos
-            talentos no cenário inclusivo de VALORANT.
-          </p>
-        </div>
+          <h1 className={`text-8xl yellow`}>OLÁ!</h1>
 
-        <div className="flex flex-col sm:flex-row gap-10 items-center">
-          <Button label="Saiba mais" outline bigButton onClick={openLinkTree} />
-          <Link href="/leaderboard">
-            <Button label="Ver Leaderboard" outline bigButton />
-          </Link>
+          <div
+            className={`text-center my-8 flex flex-col gap-y-4 ${styles.presentationText}`}
+            style={{ maxWidth: '40rem' }}
+          >
+            <p>
+              Bem-vindo(a) a <b>HUB Inclusivo</b> de <b>VALORANT!</b>
+            </p>
+            <p>
+              Somos uma comunidade dedicada a promover a competitividade e
+              evolução das jogadoras do cenário inclusivo de VALORANT.
+            </p>
+            <p>
+              Acreditamos na importância de proporcionar um ambiente amigável
+              onde as mulheres cis, trans e pessoas não-binárias possam se
+              sentir representadas e respeitadas.
+            </p>
+            <p>
+              A missão da HUB é garantir um ambiente altamente competitivo,
+              melhorando a descoberta, o desenvolvimento e a promoção de novos
+              talentos no cenário inclusivo de VALORANT.
+            </p>
+          </div>
+
+          <div className="flex flex-col sm:flex-row gap-10 items-center">
+            <Button
+              label="Saiba mais"
+              outline
+              bigButton
+              onClick={openLinkTree}
+            />
+            <Link href="/leaderboard">
+              <Button label="Ver Leaderboard" outline bigButton />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- packages/web/src/pages/leaderboard/index.tsx
+++ packages/web/src/pages/leaderboard/index.tsx
@@ -16,6 +16,7 @@ import styles from './styles.module.scss'
 import TrophyTOP1 from '../../shared/assets/img/trophies/trophy-top1.svg'
 import TrophyTOP2 from '../../shared/assets/img/trophies/trophy-top2.svg'
 import TrophyTOP3 from '../../shared/assets/img/trophies/trophy-top3.svg'
+import Head from 'next/head'
 
 interface LeaderboardProps {
   leaderboard: Leaderboard[]
@@ -47,106 +48,59 @@ export default function Leaderboard({ leaderboard }: LeaderboardProps) {
   }
 
   return (
-    <div className="h-full">
-      <Link className={`absolute ${styles.arrowBack}`} href="/">
-        <BsFillArrowLeftCircleFill className="text-4xl" />
-      </Link>
+    <>
+      <Head>
+        <title>HUBI - Leaderboard</title>
+      </Head>
+      <div className="h-full">
+        <Link className={`absolute ${styles.arrowBack}`} href="/">
+          <BsFillArrowLeftCircleFill className="text-4xl" />
+        </Link>
 
-      <div className="flex justify-center h-full items-center">
-        <div className="flex flex-col items-center w-full lg:max-w-5xl mt-16">
-          <h1 className={`text-6xl sm:text-8xl`}>LEADERBOARD</h1>
+        <div className="flex justify-center h-full items-center">
+          <div className="flex flex-col items-center w-full lg:max-w-5xl mt-16">
+            <h1 className={`text-6xl sm:text-8xl`}>LEADERBOARD</h1>
 
-          <div className="flex flex-col sm:flex-row w-full justify-center items-center">
-            <Image
-              src={seasons}
-              alt="Season atual"
-              className="block sm:hidden mt-2"
-            />
-            <input
-              className={`${styles.input} my-5 sm:m-10 w-10/12 sm:max-w-lg focus:outline-none`}
-              name="player"
-              placeholder="Digite o nick de uma jogadora e aperte enter para pesquisar..."
-              onChange={(event) => setNickname(event.target.value)}
-              onKeyDown={(event) => handleSearch(event.key)}
-            />
-            <Image
-              src={seasons}
-              alt="Season atual"
-              className="hidden sm:block"
-            />
-          </div>
-
-          <div className={`${styles.table} w-full p-3 overflow-auto`}>
-            <div className="grid grid-cols-12 text-center">
-              <p className="col-span-3 md:col-span-2 text-sm sm:text-xl">
-                CLASSIFICAÇÃO
-              </p>
-              <p className="col-span-3 md:col-span-3 text-sm  sm:text-xl text-green-500">
-                PONTUAÇÃO
-              </p>
-              <p className="col-span-6 md:col-span-4 text-sm sm:text-xl"></p>
-              <p className="md:col-span-3 text-xs hidden md:block sm:text-xl">
-                PARTIDAS
-              </p>
+            <div className="flex flex-col sm:flex-row w-full justify-center items-center">
+              <Image
+                src={seasons}
+                alt="Season atual"
+                className="block sm:hidden mt-2"
+              />
+              <input
+                className={`${styles.input} my-5 sm:m-10 w-10/12 sm:max-w-lg focus:outline-none`}
+                name="player"
+                placeholder="Digite o nick de uma jogadora e aperte enter para pesquisar..."
+                onChange={(event) => setNickname(event.target.value)}
+                onKeyDown={(event) => handleSearch(event.key)}
+              />
+              <Image
+                src={seasons}
+                alt="Season atual"
+                className="hidden sm:block"
+              />
             </div>
-            {player ? (
-              <div
-                w-full
-                key={player.userId}
-                className={`${styles.tableContent} h-20 rounded-b-3xl ${styles.tableFirstRow} mt-3 text-center grid grid-cols-12 gap-6 items-center`}
-              >
-                <p className="col-span-3 md:col-span-2">{player.position}</p>
-                <p className="col-span-3 md:col-span-3">{player.points}</p>
-                <p className="col-span-6 md:col-span-4 ">{player.nickname}</p>
-                <p className="md:col-span-3 hidden md:block">
-                  {player.played} {player.played == 1 ? 'Partida' : 'Partidas'}{' '}
+
+            <div className={`${styles.table} w-full p-3 overflow-auto`}>
+              <div className="grid grid-cols-12 text-center">
+                <p className="col-span-3 md:col-span-2 text-sm sm:text-xl">
+                  CLASSIFICAÇÃO
+                </p>
+                <p className="col-span-3 md:col-span-3 text-sm  sm:text-xl text-green-500">
+                  PONTUAÇÃO
+                </p>
+                <p className="col-span-6 md:col-span-4 text-sm sm:text-xl"></p>
+                <p className="md:col-span-3 text-xs hidden md:block sm:text-xl">
+                  PARTIDAS
                 </p>
               </div>
-            ) : (
-              leaderboard.length > 0 &&
-              leaderboard.map((player, index) => (
+              {player ? (
                 <div
                   w-full
                   key={player.userId}
-                  className={`${styles.tableContent} ${
-                    index == 0
-                      ? `h-20 rounded-b-3xl ${styles.tableFirstRow}`
-                      : 'h-10 rounded-full'
-                  } mt-3 text-center grid grid-cols-12 gap-6 items-center`}
+                  className={`${styles.tableContent} h-20 rounded-b-3xl ${styles.tableFirstRow} mt-3 text-center grid grid-cols-12 gap-6 items-center`}
                 >
-                  <div className="col-span-3 md:col-span-2 flex gap-5 w-full justify-center">
-                    <p
-                      className={`${
-                        (index === 0 || index === 1 || index === 2) && 'pl-10'
-                      }`}
-                    >
-                      {index + 1}
-                    </p>
-                    {index == 0 ? (
-                      <Image
-                        src={TrophyTOP1}
-                        alt="TOP 1"
-                        width={30}
-                        height={30}
-                      />
-                    ) : index == 1 ? (
-                      <Image
-                        src={TrophyTOP2}
-                        alt="TOP 2"
-                        width={20}
-                        height={20}
-                      />
-                    ) : index == 2 ? (
-                      <Image
-                        src={TrophyTOP3}
-                        alt="TOP 3"
-                        width={20}
-                        height={20}
-                      />
-                    ) : (
-                      ''
-                    )}
-                  </div>
+                  <p className="col-span-3 md:col-span-2">{player.position}</p>
                   <p className="col-span-3 md:col-span-3">{player.points}</p>
                   <p className="col-span-6 md:col-span-4 ">{player.nickname}</p>
                   <p className="md:col-span-3 hidden md:block">
@@ -154,12 +108,67 @@ export default function Leaderboard({ leaderboard }: LeaderboardProps) {
                     {player.played == 1 ? 'Partida' : 'Partidas'}{' '}
                   </p>
                 </div>
-              ))
-            )}
+              ) : (
+                leaderboard.length > 0 &&
+                leaderboard.map((player, index) => (
+                  <div
+                    w-full
+                    key={player.userId}
+                    className={`${styles.tableContent} ${
+                      index == 0
+                        ? `h-20 rounded-b-3xl ${styles.tableFirstRow}`
+                        : 'h-10 rounded-full'
+                    } mt-3 text-center grid grid-cols-12 gap-6 items-center`}
+                  >
+                    <div className="col-span-3 md:col-span-2 flex gap-5 w-full justify-center">
+                      <p
+                        className={`${
+                          (index === 0 || index === 1 || index === 2) && 'pl-10'
+                        }`}
+                      >
+                        {index + 1}
+                      </p>
+                      {index == 0 ? (
+                        <Image
+                          src={TrophyTOP1}
+                          alt="TOP 1"
+                          width={30}
+                          height={30}
+                        />
+                      ) : index == 1 ? (
+                        <Image
+                          src={TrophyTOP2}
+                          alt="TOP 2"
+                          width={20}
+                          height={20}
+                        />
+                      ) : index == 2 ? (
+                        <Image
+                          src={TrophyTOP3}
+                          alt="TOP 3"
+                          width={20}
+                          height={20}
+                        />
+                      ) : (
+                        ''
+                      )}
+                    </div>
+                    <p className="col-span-3 md:col-span-3">{player.points}</p>
+                    <p className="col-span-6 md:col-span-4 ">
+                      {player.nickname}
+                    </p>
+                    <p className="md:col-span-3 hidden md:block">
+                      {player.played}{' '}
+                      {player.played == 1 ? 'Partida' : 'Partidas'}{' '}
+                    </p>
+                  </div>
+                ))
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
