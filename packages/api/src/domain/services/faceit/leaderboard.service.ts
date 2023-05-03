52e60852faf7cce8cfb3bd36beeef7e745083455
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-03 18:55:23 -0300

    feat: return position to leaderboard too

--- packages/api/src/domain/services/faceit/leaderboard.service.ts
+++ packages/api/src/domain/services/faceit/leaderboard.service.ts
@@ -19,6 +19,7 @@ export class LeaderboardService {
       userId: item.player.user_id,
       nickname: item.player.nickname,
       played: item.played,
+      position: item.position,
       points: item.points,
     }))
   }
