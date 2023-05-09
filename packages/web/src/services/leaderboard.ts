30b6b7e947be6530000bf36382dba98cceb335a8
Vinicius Marinho <viniciusalexsander1@gmail.com>
2023-05-08 22:33:45 -0300

    adicionando select para obter season

--- packages/web/src/services/leaderboard.ts
+++ packages/web/src/services/leaderboard.ts
@@ -1,12 +1,19 @@
-import { PlayerLeaderboard } from '@hubi/types'
+import { Leaderboard, PlayerLeaderboard } from '@hubi/types'
 import { api } from './api'
 
-export async function getLeaderboard(): Promise<PlayerLeaderboard[]> {
+export async function getLeaderboard(
+  leaderboardId: string,
+): Promise<PlayerLeaderboard[]> {
   const response = await api.get<PlayerLeaderboard[]>('/leaderboard', {
     params: {
       limit: 50,
-      season: 1,
+      season: leaderboardId,
     },
   })
+  return response.data
+}
+
+export async function getListLeaderboards(): Promise<Leaderboard[]> {
+  const response = await api.get<Leaderboard[]>('/hub/leaderboards')
   return response.data
 }
