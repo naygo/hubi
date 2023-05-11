f90e1251f089c9c843e7e0387f870bbe445cde40
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-11 19:37:00 -0300

    chore: limpando projeto para iniciar nova fase do site

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,10 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ToastContainer } from 'react-toastify'
 
-import { Footer } from '@/shared/components/footer'
-
-import styles from './styles.module.scss'
 import '../styles/globals.scss'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -17,19 +13,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
         />
       </Head>
-      <ToastContainer />
-      <div
-        className={`${styles.leftBackground} opacity-40 lg:opacity-70 xl:opacity-100 max-w-full sm:max-w-none `}
-      ></div>
-      <div
-        className={`${styles.rightBackground} hidden sm:block opacity-20 lg:opacity-70 xl:opacity-100`}
-      ></div>
-      <div style={{ minHeight: '89vh' }}>
-        <Component {...pageProps} />
-      </div>
-      <div style={{ height: '11vh' }}>
-        <Footer />
-      </div>
+      <Component {...pageProps} />
     </>
   )
 }
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,13 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
-
-import { Button } from '@/shared/components/Button'
-
-import styles from './styles.module.scss'
-
-function openLinkTree() {
-  window.open('https://linktr.ee/hub.inclusivo', '_blank')
-}
 
 export default function Home() {
   return (
@@ -15,48 +6,7 @@ export default function Home() {
       <Head>
         <title>HUBI</title>
       </Head>
-      <div className="flex flex-col justify-between">
-        <div
-          className={`flex flex-col justify-center items-center mt-16 gap-y-6 p-3`}
-        >
-          <h1 className={`text-8xl yellow`}>OLÁ!</h1>
-
-          <div
-            className={`text-center my-8 flex flex-col gap-y-4 ${styles.presentationText}`}
-            style={{ maxWidth: '40rem' }}
-          >
-            <p>
-              Bem-vindo(a) a <b>HUB Inclusivo</b> de <b>VALORANT!</b>
-            </p>
-            <p>
-              Somos uma comunidade dedicada a promover a competitividade e
-              evolução das jogadoras do cenário inclusivo de VALORANT.
-            </p>
-            <p>
-              Acreditamos na importância de proporcionar um ambiente amigável
-              onde as mulheres cis, trans e pessoas não-binárias possam se
-              sentir representadas e respeitadas.
-            </p>
-            <p>
-              A missão da HUB é garantir um ambiente altamente competitivo,
-              melhorando a descoberta, o desenvolvimento e a promoção de novos
-              talentos no cenário inclusivo de VALORANT.
-            </p>
-          </div>
-
-          <div className="flex flex-col sm:flex-row gap-10 items-center">
-            <Button
-              label="Saiba mais"
-              outline
-              bigButton
-              onClick={openLinkTree}
-            />
-            <Link href="/leaderboard">
-              <Button label="Ver Leaderboard" outline bigButton />
-            </Link>
-          </div>
-        </div>
-      </div>
+      <h1>HUBI</h1>
     </>
   )
 }
