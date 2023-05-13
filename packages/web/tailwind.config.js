3e02151c4f40728889fcaabfcf2137ae3153fe30
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-13 16:43:14 -0300

    feat: congi custom color and keeping tailwind colors

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -6,24 +6,26 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      black: {
-        light: '#1E1C1F',
-        DEFAULT: '#0B0B0B',
-      },
-      white: {
-        DEFAULT: '#e5e5e5',
-      },
-      yellow: {
-        light: '#f5e6c4',
-        DEFAULT: '#e09600',
-      },
-    },
     borderColor: (theme) => ({
       ...theme('colors'),
     }),
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        black: {
+          light: '#1E1C1F',
+          DEFAULT: '#0B0B0B',
+        },
+        white: {
+          DEFAULT: '#e5e5e5',
+        },
+        yellow: {
+          light: '#f5e6c4',
+          DEFAULT: '#e09600',
+        },
+      },
     },
     plugins: [],
   },
