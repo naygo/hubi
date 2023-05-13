f5c4c3c838fe6a5dcd9eac156162df5364120cbd
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-13 18:51:39 -0300

    feat: fixing types

--- packages/web/src/shared/components/dropdown/index.tsx
+++ packages/web/src/shared/components/dropdown/index.tsx
@@ -1,15 +1,18 @@
 import { Listbox } from '@headlessui/react'
-import { IoCaretDownSharp, IoAlertCircleOutline } from 'react-icons/io5'
 import { useController, RegisterOptions, Control } from 'react-hook-form'
+
+import { IoCaretDownSharp, IoAlertCircleOutline } from 'react-icons/io5'
+
 import { generateStyleButton } from './dropdown.styles'
 
 interface DropDownProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   control: Control<any>
   name: string
   placeholder: string
   options: {
-    label: any
-    value: any
+    label: string
+    value: unknown
   }[]
   rules: Omit<
     RegisterOptions,
@@ -42,7 +45,7 @@ export function DropDown({
         <Listbox.Options className="bg-black-light rounded-lg py-2 mt-1">
           {options.map((option) => (
             <Listbox.Option
-              key={option.value}
+              key={option.label}
               value={option.value}
               className="hover:bg-yellow px-2 py-1 cursor-pointer bg-black-light"
             >
@@ -53,10 +56,7 @@ export function DropDown({
       </Listbox>
       {fieldState.error?.message && (
         <span className="text-red-500 text-xs flex items-center">
-          <IoAlertCircleOutline
-            size={16}
-            className="mr-1"
-          />
+          <IoAlertCircleOutline size={16} className="mr-1" />
           {fieldState.error?.message}
         </span>
       )}
