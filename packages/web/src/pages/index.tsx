7a3e6388a4350f36eb74e23b330e81df92b8dca4
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-13 19:00:38 -0300

    feat: add components on page

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,12 +1,71 @@
 import Head from 'next/head'
+import { useForm } from 'react-hook-form'
+
+import { InputText } from '@/shared/components/inputText'
+import { Dropdown } from '@/shared/components/dropdown'
 
 export default function Home() {
+  const { control, handleSubmit, formState } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  })
+
   return (
     <>
       <Head>
         <title>HUBI</title>
       </Head>
-      <h1>HUBI</h1>
+
+      <div className="mt-5 w-screen flex flex-col justify-center items-center">
+        <h1>HUBI</h1>
+
+        <div className="flex flex-col gap-y-5">
+          <div>
+            <label htmlFor="inputText">InputText:</label>
+            <InputText
+              placeholder="Placeholder do inputText"
+              control={control}
+              rules={{
+                required: {
+                  value: true,
+                  message: '',
+                },
+              }}
+              name="inputText"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="dropdown">Dropdown:</label>
+            <Dropdown
+              {...{
+                control,
+                name: 'dropdown',
+                placeholder: 'Placeholder do Dropdown',
+                options: [
+                  {
+                    label: 'Label 1',
+                    value: 1,
+                  },
+                  {
+                    label: 'Label 2',
+                    value: 2,
+                  },
+                ],
+                rules: {
+                  required: {
+                    value: true,
+                    message: '',
+                  },
+                },
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
