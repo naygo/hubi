ae4ee2d69b76bd737193281ec3e6a7b8552307de
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-15 22:30:34 -0300

    chore: add popping regular italic font

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -12,9 +12,9 @@ export default function Document() {
           crossOrigin=""
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,800&display=swap"
           rel="stylesheet"
-        ></link>
+        />
 
         {/* Icon */}
         <link rel="shortcut icon" href="/favicon.ico" />
