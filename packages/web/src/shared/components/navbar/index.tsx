5cfed690f8300d0f0c41401dcc690689a99784c6
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-18 20:32:19 -0300

    feat: add style accordingly with the active route

--- packages/web/src/shared/components/navbar/index.tsx
+++ packages/web/src/shared/components/navbar/index.tsx
@@ -1,36 +1,54 @@
+import clsx from 'clsx'
 import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { IoInvertMode } from 'react-icons/io5'
 
 import { LogoNouns } from '@/shared/icons/LogoNouns'
+import { routes } from '@/shared/utils/routes'
 
 import Logo from '../../../../public/img/logo.svg'
 import { Button } from '../button'
 
+const noundAndDarkModeButtons =
+  'bg-black-lighter hover:bg-black-light cursor-pointer rounded p-1'
+
+const arrayRoutes = [
+  { name: 'Página Inicial', route: routes.home },
+  { name: 'Leaderboard', route: routes.leaderboard },
+  { name: 'Playground', route: routes.playground },
+]
+
 export function Navbar() {
-  const noundAndDarkModeButtons =
-    'bg-black-lighter hover:bg-black-light cursor-pointer rounded p-1'
+  const pathname = usePathname()
 
   return (
     <nav className="w-full flex gap-4 justify-between items-center p-4 border-b border-black-lighter font-normal text-sm">
       <div className="flex items-center">
         <Image src={Logo} width={100} height={100} alt="HUBI Logo" />
-
         {/* linha vertical */}
         <div className="h-8 w-0.5 bg-black-lighter mx-5"></div>
 
         {/* links */}
         <div className="flex gap-4 text-gray">
-          <a href="" className="hover:text-yellow">
-            Página Inicial
-          </a>
-          <a href="" className="hover:text-yellow">
-            Leaderboard
-          </a>
-
-          <a href="" className="font-bold text-white">
-            Playground
-          </a>
+          {arrayRoutes.map((routes) => (
+            <div
+              key={routes.route}
+              className={clsx({
+                'pointer-events-none': pathname === routes.route,
+              })}
+            >
+              <Link
+                href={routes.route}
+                className={clsx('hover:text-yellow', {
+                  'font-bold text-white': pathname === routes.route,
+                })}
+              >
+                {routes.name}
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
 
