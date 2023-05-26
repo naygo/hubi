8ff068704fcd3b779d1b3ea5863b6d29186d4b26
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-26 20:35:53 -0300

    feat: add page singup

---
+++ packages/web/src/pages/singup/index.tsx
@@ -0,0 +1,33 @@
+// icone de triangulo para a direita
+import { AiFillCaretRight } from 'react-icons/ai'
+export default function Singup() {
+  return (
+    <main className="flex flex-col h-screen w-screen">
+      <div className="flex items-center gap-5">
+        <div className="">
+          <p>Etapa 1</p>
+          <div className="bg-gray-darker h-1 rounded"></div>
+        </div>
+        <AiFillCaretRight size={10} fill="gray" />
+        <div className="">
+          <p>Etapa 2</p>
+          <div className="bg-gray-darker h-1 rounded"></div>
+        </div>
+        <AiFillCaretRight size={10} fill="gray" />
+        <div className="">
+          <p>Etapa 3</p>
+          <div className="bg-gray-darker h-1 rounded"></div>
+        </div>
+      </div>
+
+      <section className="w-full">
+        <h1>Crie sua conta na HUBI</h1>
+        <p className="text-gray text-sm">
+          O seu cadastro irá passar por um processo de verificação e pode
+          demorar alguns instantes para ser aprovado.
+        </p>
+      </section>
+      <section className="hidden md:block w-full">logo</section>
+    </main>
+  )
+}
