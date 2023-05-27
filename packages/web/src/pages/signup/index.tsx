3c2183626bcb7b2d93a1dfd715f1446340c9005b
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-26 22:48:00 -0300

    feat: adjust breakpoints to not break the title in small devices

--- packages/web/src/pages/signup/index.tsx
+++ packages/web/src/pages/signup/index.tsx
@@ -15,27 +15,27 @@ export default function SignUp() {
         <section className="w-full">
           <div className="flex items-center gap-5 ml-5 mb-5">
             <ReactLink href={''}>
-              <p className="text-sm">Etapa 1</p>
+              <p className="text-sm md:text-base">Etapa 1</p>
               <div className="bg-yellow h-1 mt-1 rounded"></div>
             </ReactLink>
             <AiFillCaretRight size={10} fill={colors.grayDarker} />
             <ReactLink href={''}>
-              <p className="text-sm">Etapa 2</p>
+              <p className="text-sm md:text-base">Etapa 2</p>
               <div className="bg-gray-darker h-1 mt-1 rounded"></div>
             </ReactLink>
             <AiFillCaretRight size={10} fill={colors.grayDarker} />
             <ReactLink href={''}>
-              <p className="text-sm">Etapa 3</p>
+              <p className="text-sm md:text-base">Etapa 3</p>
               <div className="bg-gray-darker h-1 mt-1 rounded"></div>
             </ReactLink>
           </div>
 
           <div className="w-full bg-black-lighter rounded-3xl p-8">
-            <div className="flex gap-2.5">
-              <h1 className="font-bold text-xl sm:text-2xl lg:text-3xl align-bottom">
+            <div className="flex gap-1 md:gap-1.5">
+              <h1 className="font-bold text-lg sm:text-2xl lg:text-3xl align-bottom">
                 Crie sua conta na
               </h1>
-              <Image src={Logo} alt="HUBI" className="w-14 sm:w-16 lg:w-20" />
+              <Image src={Logo} alt="HUBI" className="w-12 sm:w-16 lg:w-20" />
             </div>
             <p className="text-gray text-xs lg:text-sm mt-2 mb-10 text-justify">
               O seu cadastro irá passar por um processo de verificação e pode
