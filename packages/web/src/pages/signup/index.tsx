24f628668c0ca99d70f0400bf145bdd35481bcc1
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-27 12:14:24 -0300

    feat: add right info section in mobile and desk devices

--- packages/web/src/pages/signup/index.tsx
+++ packages/web/src/pages/signup/index.tsx
@@ -10,6 +10,8 @@ import { StepOne } from '@/screens/signup/step-1'
 import { StepTwo } from '@/screens/signup/step-2'
 import { StepThree } from '@/screens/signup/step-3'
 import { Link } from '@/shared/components/ui/link'
+import SocialMediaLinks from '@/shared/components/ui/social-media-links'
+import styles from '@/styles/classes'
 import colors from '@/styles/colors'
 
 import Logo from '@public/img/logo.svg'
@@ -27,16 +29,29 @@ export default function SignUp() {
       <Head>
         <title>Cadastro | HUBI</title>
       </Head>
-      <div className="container flex flex-col p-5 md:flex-row">
-        <section className="md:w-full lg:w-8/12">
+      <div className="container flex flex-col p-5 lg:flex-row">
+        <div className="flex flex-col items-center lg:hidden">
+          <Image
+            src={Logo}
+            alt="Picture of the author"
+            width={200}
+            className={`${styles.logoBox} w-80`}
+          />
+          <div className="text-xs md:text-sm text-center mt-2 mb-10">
+            Somos uma plataforma e comunidade de players do cenário inclusivo de
+            VALORANT.
+          </div>
+        </div>
+
+        <section className="md:w-full lg:w-10/12">
           <Steps currentStep={currentStep} handleNextStep={handleNextStep} />
 
           <div className="w-full bg-black-light rounded-3xl p-8">
             <div className="flex gap-1 md:gap-1.5">
-              <h1 className="font-bold text-lg sm:text-2xl align-bottom">
+              <h1 className="font-bold text-lg sm:text-2xl lg:text-3xl align-bottom">
                 Crie sua conta na
               </h1>
-              <Image src={Logo} alt="HUBI" className="w-12 sm:w-16" />
+              <Image src={Logo} alt="HUBI" className="w-12 sm:w-16 lg:w-20" />
             </div>
             <p className="text-gray text-xs lg:text-sm mt-2 mb-10 text-justify">
               O seu cadastro irá passar por um processo de verificação e pode
@@ -67,8 +82,23 @@ export default function SignUp() {
             </div>
           </div>
         </section>
-        <section className="hidden md:block w-full">
-          logo and social media buttons
+
+        <section className="flex flex-col items-center mt-2 w-full lg:items-center lg:justify-center">
+          <div className="lg:items-start">
+            <div className="hidden lg:block">
+              <Image
+                src={Logo}
+                alt="HUBI"
+                width={200}
+                className={`${styles.logoBox} w-80`}
+              />
+              <div className="lg:w-96 lg:mt-2">
+                Somos uma plataforma e comunidade de players do cenário
+                inclusivo de VALORANT.
+              </div>
+            </div>
+            <SocialMediaLinks />
+          </div>
         </section>
       </div>
     </main>
