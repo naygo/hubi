5f6dffd18b328f2fd6cc40ddc2573f549b48825f
Ives M. W <ivesmw@gmail.com>
2023-05-30 20:13:52 -0300

    refactor: use AuthCard in login

--- packages/web/src/screens/login/form/index.tsx
+++ packages/web/src/screens/login/form/index.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image'
 import { useForm } from 'react-hook-form'
 
 import { Input } from '@/shared/components/form/input'
 import { Button } from '@/shared/components/ui/button'
+import { Link } from '@/shared/components/ui/link'
 import { useYupValidationResolver } from '@/shared/hooks/useYupValidationResolver'
 import { LoginFormFields } from '@/shared/types/login-form'
+import { routes } from '@/shared/utils/routes'
 
 import { validationSchema } from './validators'
+
+import Logo from '@public/img/logo.svg'
 
 export function LoginForm() {
   const resolver = useYupValidationResolver(validationSchema)
@@ -19,33 +24,53 @@ export function LoginForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit(handleSendForm)} noValidate>
-      <div className="flex flex-col gap-2">
-        <Input
-          name="email"
-          label="E-mail:"
-          type="email"
-          control={control}
-          rules={{ required: true }}
-        />
+    <section className="md:w-full lg:w-10/12">
+      <div className="w-full bg-black-light rounded-3xl p-8">
+        <div className="flex gap-1 md:gap-1.5 mb-10">
+          <h1 className="font-bold text-lg sm:text-2xl lg:text-3xl align-bottom">
+            Faça login na
+          </h1>
+          <Image src={Logo} alt="HUBI" className="w-12 sm:w-16 lg:w-20" />
+        </div>
 
-        <Input
-          name="password"
-          label="Senha:"
-          type="password"
-          control={control}
-          rules={{ required: true }}
-        />
-      </div>
+        <form onSubmit={handleSubmit(handleSendForm)} noValidate>
+          <div className="flex flex-col gap-2">
+            <Input
+              name="email"
+              label="E-mail:"
+              type="email"
+              control={control}
+              rules={{ required: true }}
+            />
 
-      <div className="flex gap-5">
-        <Button
-          color="primary"
-          label="Entrar"
-          className="w-full my-5"
-          type="submit"
-        />
+            <Input
+              name="password"
+              label="Senha:"
+              type="password"
+              control={control}
+              rules={{ required: true }}
+            />
+          </div>
+
+          <div className="flex gap-5">
+            <Button
+              color="primary"
+              label="Entrar"
+              className="w-full my-5"
+              type="submit"
+            />
+          </div>
+        </form>
+
+        <div className="text-center text-xs md:text-sm">
+          Ainda não possui uma conta?{'  '}
+          <Link
+            text="Crie uma aqui &gt;"
+            link={routes.signup}
+            className="font-medium"
+          />
+        </div>
       </div>
-    </form>
+    </section>
   )
 }
