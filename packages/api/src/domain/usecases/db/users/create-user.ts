3214ad927a74566f51291e87557ad596f6241b05
Cauã Henrique <cauah6002@gmail.com>
2023-06-04 13:56:45 -0300

    feat: hash password and create socials of user and make validations

--- packages/api/src/domain/usecases/db/users/create-user.ts
+++ packages/api/src/domain/usecases/db/users/create-user.ts
@@ -1,27 +1,79 @@
+import { User, UserSocial, UserStatusEnum } from '@hubi/types'
+import { Hasher } from '@/infra/cryptography'
+import { UserSocialsRepository } from '@/infra/db/prisma/repositories/user-socials.repository'
 import { UsersRepository } from '@/infra/db/prisma/repositories/users.repository'
-import { User } from '@hubi/types/db'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class CreateUser {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly userSocialsRepository: UserSocialsRepository,
+    private readonly hasher: Hasher,
+  ) {}
 
   async execute(parameters: CreateUser.Parameters): Promise<CreateUser.Result> {
-    let valid = true
+    const result: CreateUser.Result = {
+      valid: true,
+      message: null,
+    }
+
+    if (parameters.password !== parameters.confirmPassword) {
+      Object.assign(result, {
+        message: 'Senha Inválida',
+        valid: false,
+      })
+
+      return result
+    }
+
+    const userInDb = await this.usersRepository.loadByEmail({
+      email: parameters.email,
+    })
+
+    if (userInDb) {
+      Object.assign(result, {
+        message: 'Já existe um usuário com esse email',
+        valid: false,
+      })
+
+      return result
+    }
+
+    const { socials, confirmPassword, ...user } = parameters
+    const hashedPassword = await this.hasher.hash(user.password)
 
     const userToCreate: UsersRepository.CreateUserParameters = {
-      ...parameters,
+      ...user,
       createdAt: new Date(),
       isAdmin: false,
+      password: hashedPassword,
+      status: UserStatusEnum.CREATED,
     }
 
-    const user = await this.usersRepository.create(userToCreate)
+    const createdUser = await this.usersRepository.create(userToCreate)
 
-    if (!user) {
-      valid = false
+    if (!createdUser) {
+      Object.assign(result, {
+        message: 'Não foi possível criar esse usuário',
+        valid: false,
+      })
+
+      return result
     }
 
-    return valid
+    const socialsToCreate: UserSocialsRepository.CreateUserParameters =
+      socials.map((social) => ({
+        socialId: social.socialId,
+        url: social.url,
+        userId: createdUser.id,
+        createdAt: new Date(),
+        status: 1,
+      }))
+
+    await this.userSocialsRepository.create(socialsToCreate)
+
+    return result
   }
 }
 
@@ -29,6 +81,9 @@ export namespace CreateUser {
   export type Parameters = Omit<
     User,
     'id' | 'createdAt' | 'isAdmin' | 'gender' | 'pronoun' | 'rank' | 'socials'
-  >
-  export type Result = boolean
+  > & { socials: Omit<UserSocial, 'id' | 'userId'>[]; confirmPassword: string }
+  export type Result = {
+    valid: boolean
+    message?: string
+  }
 }
