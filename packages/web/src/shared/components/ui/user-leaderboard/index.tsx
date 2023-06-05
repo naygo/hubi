6e83ac7dc0c709f9846e469d25bd94ae5c37682f
Ives M. W <ivesmw@gmail.com>
2023-06-05 20:25:32 -0300

    style: user card

--- packages/web/src/shared/components/ui/user-leaderboard/index.tsx
+++ packages/web/src/shared/components/ui/user-leaderboard/index.tsx
@@ -7,6 +7,8 @@ import { getCurrentLeaderboard, getLeaderboard } from '@/services/leaderboard'
 
 import User from '@public/img/tmp/dog.jpg'
 
+const CURRENT_USER_MOCK_ID = 'a9402ddc-81b3-46b2-b38d-9f6476f77813'
+
 const playerMatches = (player: PlayerLeaderboard, userId: string) =>
   player.userId === userId
 
@@ -33,24 +35,23 @@ export function UserLeaderboard() {
   return (
     <div className="flex flex-col gap-2">
       {leaderboard &&
-        leaderboard.map((player, index) => (
-          <UserCard
-            key={player.nickname}
-            player={player}
-            className={clsx('bg-gradient-to-r to-bg-black', {
-              'from-[#e5c97d80]': index === 0,
-              'from-[#82828280]': index === 1,
-              'from-[#C4947D80]': index === 2,
-              'from-black-lighter':
-                index > 2 &&
-                !playerMatches(player, '72293acc-8eea-4186-bc2c-ace441428dec'),
-              'from-[#E0960080]': playerMatches(
-                player,
-                '72293acc-8eea-4186-bc2c-ace441428dec',
-              ),
-            })}
-          />
-        ))}
+        leaderboard.map((player) => {
+          const isCurrentUser = playerMatches(player, CURRENT_USER_MOCK_ID)
+
+          return (
+            <UserCard
+              key={player.userId}
+              player={player}
+              className={clsx('bg-gradient-to-r to-bg-black rounded-br-full', {
+                'from-[#E5C97D80]': !isCurrentUser && player.position === 1,
+                'from-[#82828280]': !isCurrentUser && player.position === 2,
+                'from-[#C4947D80]': !isCurrentUser && player.position === 3,
+                'from-black-lighter': !isCurrentUser && player.position > 3,
+                'from-[#E0960080]': isCurrentUser,
+              })}
+            />
+          )
+        })}
     </div>
   )
 }
@@ -65,8 +66,24 @@ function UserCard({ player, className }: UserCardProps) {
     <div
       className={`${className} w-72 p-2 px-3 rounded flex items-center justify-start gap-2`}
     >
-      <div className="opacity-90 rounded">
+      <div className="opacity-90 rounded relative">
         <Image src={User.src} width={56} height={56} alt="user" className="" />
+        <div>
+          <div
+            className={clsx(
+              'absolute top-11 left-1/2 transform -translate-x-1/2 rounded border-2  bg-black w-8 h-4 text-xs text-center leading-[14px]',
+              {
+                'border-[#E5C97D]': player.position === 1,
+                'border-[#828282]': player.position === 2,
+                'border-[#C4947D]': player.position === 3,
+                'border-[#BBBCBD]': player.position > 3,
+                'border-[#E09600]': playerMatches(player, CURRENT_USER_MOCK_ID),
+              },
+            )}
+          >
+            {player.position}
+          </div>
+        </div>
       </div>
       <div className="text-sm">
         <div>
