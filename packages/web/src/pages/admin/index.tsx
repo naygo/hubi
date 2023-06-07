476e6b0c7e9a636d6b0ebf3bd55aeb2c59eee382
Nayla Gomes <nayla.cgs@gmail.com>
2023-06-06 22:35:14 -0300

    feat: adjust screen to medium and large devices

--- packages/web/src/pages/admin/index.tsx
+++ packages/web/src/pages/admin/index.tsx
@@ -10,91 +10,93 @@ export default function Admin() {
     mode: 'onChange',
   })
   return (
-    <main className="flex flex-col justify-center items-center gap-5 py-6 px-4">
-      <h1 className="text-2xl font-bold text-yellow">GERENCIAR MEMBROS</h1>
-      <div className="flex flex-col justify-start items-center gap-2 text-xs w-full">
-        <div>
+    <main className="flex justify-center items-start">
+      <div className="container flex flex-col justify-center items-center gap-5 lg:gap-10 py-6 px-4">
+        <h1 className="text-2xl lg:text-5xl font-bold text-yellow text-center">
+          GERENCIAR MEMBROS
+        </h1>
+        <div className="flex flex-col md:flex-row justify-start md:justify-center items-center gap-2 text-xs w-full">
           <Input
             name="search"
             control={control}
             placeholder="Pesquisar membro"
           />
+
+          <div className="flex gap-2">
+            <button onClick={() => setStatusFilter('Aprovada')}>
+              <Badge
+                type="Aprovada"
+                filter
+                active={statusFilter === 'Aprovada'}
+              />
+            </button>
+            <button onClick={() => setStatusFilter('Pendente')}>
+              <Badge
+                type="Pendente"
+                filter
+                active={statusFilter === 'Pendente'}
+              />
+            </button>
+            <button onClick={() => setStatusFilter('Recusada')}>
+              <Badge
+                type="Recusada"
+                filter
+                active={statusFilter === 'Recusada'}
+              />
+            </button>
+          </div>
         </div>
+        <div className="overflow-auto w-full rounded">
+          <table className="text-md text-left w-full rounded">
+            <thead className="text-xs lg:text-base uppercase bg-black-lighter">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Nome
+                </th>
+                <th scope="col" className="px-6 py-3 text-center">
+                  Status
+                </th>
+              </tr>
+            </thead>
+            <tbody className="text-sm lg:text-lg">
+              <tr className="bg-white border-b hover:bg-gray-light hover:cursor-pointer">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-black whitespace-nowrap"
+                >
+                  Mulher linda
+                </th>
+                <td className="px-6 py-4 text-center">
+                  <Badge type="Aprovada" />
+                </td>
+              </tr>
 
-        <div className="flex gap-2">
-          <button onClick={() => setStatusFilter('Aprovada')}>
-            <Badge
-              type="Aprovada"
-              filter
-              active={statusFilter === 'Aprovada'}
-            />
-          </button>
-          <button onClick={() => setStatusFilter('Pendente')}>
-            <Badge
-              type="Pendente"
-              filter
-              active={statusFilter === 'Pendente'}
-            />
-          </button>
-          <button onClick={() => setStatusFilter('Recusada')}>
-            <Badge
-              type="Recusada"
-              filter
-              active={statusFilter === 'Recusada'}
-            />
-          </button>
+              <tr className="bg-white border-b hover:bg-gray-light hover:cursor-pointer">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-black whitespace-nowrap"
+                >
+                  Mulher feia
+                </th>
+                <td className="px-6 py-4 text-center">
+                  <Badge type="Pendente" />
+                </td>
+              </tr>
+
+              <tr className="bg-white border-b hover:bg-gray-light hover:cursor-pointer">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Macho feio
+                </th>
+                <td className="px-6 py-4 text-center">
+                  <Badge type="Recusada" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      </div>
-      <div className="overflow-auto w-full rounded">
-        <table className="text-md text-left w-full rounded">
-          <thead className="text-xs uppercase bg-black-lighter">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Nome
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Status
-              </th>
-            </tr>
-          </thead>
-          <tbody className="text-sm">
-            <tr className="bg-white border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-black whitespace-nowrap"
-              >
-                Mulher linda
-              </th>
-              <td className="px-6 py-4">
-                <Badge type="Aprovada" />
-              </td>
-            </tr>
-
-            <tr className="bg-white border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-black whitespace-nowrap"
-              >
-                Mulher feia
-              </th>
-              <td className="px-6 py-4">
-                <Badge type="Pendente" />
-              </td>
-            </tr>
-
-            <tr className="bg-white border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-              >
-                Macho feio
-              </th>
-              <td className="px-6 py-4">
-                <Badge type="Recusada" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </main>
   )
@@ -116,7 +118,7 @@ function Badge({ type, filter, active }: BadgeProps) {
   return (
     <span
       className={clsx(
-        'inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium',
+        'inline-flex items-center gap-1.5 py-1.5 px-3 rounded-full text-xs lg:text-sm font-medium',
         {
           [`${colors[type.toLowerCase()]} text-black`]: !filter,
           'text-white border border-yellow': filter && !active,
