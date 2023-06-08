b3f48d9cdb8764e339978c96b03f3151df7533c5
Cauã Henrique <cauah6002@gmail.com>
2023-06-08 14:49:06 -0300

    feat: implement dto and send correct response

--- packages/api/src/presentation/controllers/users.controller.ts
+++ packages/api/src/presentation/controllers/users.controller.ts
@@ -1,24 +1,15 @@
 import { Body, Controller, Post, Res } from '@nestjs/common'
 import { CreateUser } from '@/domain/usecases/db/users/create-user'
 import { Response } from 'express'
+import { SignupDTO } from '@/presentation/dtos/user'
 
 @Controller('/user')
 export class UserController {
   constructor(private readonly createUser: CreateUser) {}
 
   @Post('/signup')
-  async signup(@Body() body, @Res() response: Response) {
-    const result = await this.createUser.execute(body)
-
-    const responseResult = {
-      statusCode: 204,
-      message: result.message,
-    }
-
-    if (!result.valid) {
-      responseResult.statusCode = 401
-    }
-
-    response.status(responseResult.statusCode).json(responseResult)
+  async signup(@Body() body: SignupDTO, @Res() response: Response) {
+    await this.createUser.execute(body)
+    response.status(201).end()
   }
 }
