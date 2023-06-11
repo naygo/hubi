e9069f56faa1004ef46d9dcdfa9d7dd8afb91c94
Nayla Gomes <nayla.cgs@gmail.com>
2023-06-11 14:49:19 -0300

    feat: add user leaderboard in the lobby

--- packages/web/src/pages/lobby/index.tsx
+++ packages/web/src/pages/lobby/index.tsx
@@ -4,39 +4,46 @@ import { getSession } from 'next-auth/react'
 import { useState } from 'react'
 
 import { NavbarFooterLayout } from '@/shared/components/layout/navbar-footer'
+import { UserLeaderboard } from '@/shared/components/ui/user-leaderboard'
 
 export default function Lobby() {
   const [isOngoing, setIsOngoing] = useState(true)
 
   return (
     <NavbarFooterLayout>
-      <main className="flex justify-center">
-        <div className="bg-black-dark flex gap-2 p-1 w-fit rounded">
-          <div
-            onClick={() => setIsOngoing(true)}
-            className={clsx(
-              'font-medium bg-black-lighter p-2 rounded cursor-pointer transition-all duration-200',
-              {
-                'bg-black-lighter text-white': isOngoing,
-                'bg-transparent text-gray': !isOngoing,
-              },
-            )}
-          >
-            Em andamento
+      <main className="flex justify-between">
+        <section>
+          <div className="bg-black-dark flex gap-2 p-1 w-fit rounded">
+            <div
+              onClick={() => setIsOngoing(true)}
+              className={clsx(
+                'font-medium bg-black-lighter p-2 rounded cursor-pointer transition-all duration-200',
+                {
+                  'bg-black-lighter text-white': isOngoing,
+                  'bg-transparent text-gray': !isOngoing,
+                },
+              )}
+            >
+              Em andamento
+            </div>
+            <div
+              onClick={() => setIsOngoing(false)}
+              className={clsx(
+                'font-medium bg-black-lighter p-2 rounded cursor-pointer',
+                {
+                  'bg-black-lighter text-white': !isOngoing,
+                  'bg-transparent text-gray': isOngoing,
+                },
+              )}
+            >
+              Finalizadas
+            </div>
           </div>
-          <div
-            onClick={() => setIsOngoing(false)}
-            className={clsx(
-              'font-medium bg-black-lighter p-2 rounded cursor-pointer',
-              {
-                'bg-black-lighter text-white': !isOngoing,
-                'bg-transparent text-gray': isOngoing,
-              },
-            )}
-          >
-            Finalizadas
-          </div>
-        </div>
+        </section>
+
+        <section className="p-4">
+          <UserLeaderboard />
+        </section>
       </main>
     </NavbarFooterLayout>
   )
