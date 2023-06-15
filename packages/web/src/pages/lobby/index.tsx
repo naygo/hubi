f6a418aba8b2f56633f85912e9a9c4df7339ee0d
Nayla Gomes <nayla.cgs@gmail.com>
2023-06-14 22:14:46 -0300

    feat: add matches cards

--- packages/web/src/pages/lobby/index.tsx
+++ packages/web/src/pages/lobby/index.tsx
@@ -16,15 +16,31 @@ export default function Lobby() {
     <NavbarLayout>
       <div className="flex justify-between mt-3">
         <div className="w-full flex justify-center px-4">
-          <section className="container">
-            <div className="mb-4">
+          <section
+            className="container"
+            style={{ height: 'calc(100vh - 110px) ' }}
+          >
+            <div className="">
               <Image src={Header} alt="Header" className="w-full" />
             </div>
-            <div className="w-full flex justify-center">
+
+            <div className="w-full flex justify-center my-4">
               <MatchesStatusButtons
                 isOngoing={isOngoing}
                 setIsOngoing={setIsOngoing}
               />
+            </div>
+
+            <div className="lg:overflow-y-auto max-h-[calc(100vh-400px)] lg:px-4">
+              <div className="flex flex-col gap-5">
+                <MatchCard />
+                <MatchCard />
+                <MatchCard />
+                <MatchCard />
+                <MatchCard />
+                <MatchCard />
+                <MatchCard />
+              </div>
             </div>
           </section>
         </div>
@@ -59,7 +75,7 @@ function MatchesStatusButtons({
   setIsOngoing: (value: boolean) => void
 }) {
   return (
-    <div className="bg-black-dark flex gap-2 p-1 w-fit rounded">
+    <div className="bg-black-dark flex gap-2 p-1 w-fit rounded text-xs lg:text-base">
       <div
         onClick={() => setIsOngoing(true)}
         className={clsx(
@@ -83,6 +99,57 @@ function MatchesStatusButtons({
         )}
       >
         Finalizadas
+      </div>
+    </div>
+  )
+}
+
+function MatchCard() {
+  return (
+    <div className="bg-black-dark rounded flex flex-col lg:flex-row cursor-pointer">
+      <div className="w-full relative">
+        <strong className="absolute m-1 lg:m-4 lg:text-3xl">LOTTUS</strong>
+        <Image src={Header} alt="Header" className="w-full h-full" />
+      </div>
+      <div className="w-full p-2 lg:p-4 text-xs lg:text-base flex justify-between items-center">
+        <div className="flex gap-2 lg:gap-4">
+          <div className="bg-red-700 h-20 lg:h-32 w-0.5 lg:w-1 rounded"></div>
+          <div className="flex flex-col justify-center">
+            <span>jogadora 1</span>
+            <span>jogadora 2</span>
+            <span>jogadora 3</span>
+            <span>jogadora 4</span>
+            <span>jogadora 5</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:flex-row gap-2 lg:gap-8 items-center">
+          <strong className="hidden lg:block text-5xl bg-black rounded p-2 text-center">
+            13
+          </strong>
+          <div className="flex flex-col items-center">
+            <strong className="text-2xl lg:text-3xl">VS</strong>
+            <span className="text-gray text-xs">16:32</span>
+          </div>
+          <strong className="hidden lg:block text-5xl bg-black rounded p-2 text-center">
+            10
+          </strong>
+
+          <div className="lg:hidden font-bold bg-black rounded p-1 text-center">
+            13 - 10
+          </div>
+        </div>
+
+        <div className="flex gap-2 lg:gap-4">
+          <div className="flex flex-col justify-center items-end">
+            <span>jogadora 6</span>
+            <span>jogadora 7</span>
+            <span>jogadora 8</span>
+            <span>jogadora 9</span>
+            <span>jogadora 10</span>
+          </div>
+          <div className="bg-sky-600 h-20 lg:h-32 w-0.5 lg:w-1 rounded"></div>
+        </div>
       </div>
     </div>
   )
