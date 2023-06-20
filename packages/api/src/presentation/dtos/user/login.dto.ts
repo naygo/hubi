fb0499fd3af60b9770804f2ef5fc57e15f1910ab
Cauã Henrique <cauah6002@gmail.com>
2023-06-19 22:34:28 -0300

    fix: remove unnecessary validations

--- packages/api/src/presentation/dtos/user/login.dto.ts
+++ packages/api/src/presentation/dtos/user/login.dto.ts
@@ -2,9 +2,7 @@ import { MessageValidator } from '@/presentation/utils'
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 export class LoginDTO {
-  @IsString({ message: MessageValidator.string('email') })
   @IsEmail({}, { message: MessageValidator.email('email') })
-  @IsNotEmpty({ message: MessageValidator.empty('email') })
   email: string
 
   @IsString({ message: MessageValidator.string('password') })
