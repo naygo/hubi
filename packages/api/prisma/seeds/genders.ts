ad7408310cb548dadc0812a8c4cfd796fcc4b300
Ives M. W <ivesmw@gmail.com>
2023-06-19 21:57:44 -0300

    feat: add seed files

---
+++ packages/api/prisma/seeds/genders.ts
@@ -0,0 +1,42 @@
+import { Genders, PrismaClient } from '@prisma/client'
+
+import { insert } from './utils/insert'
+
+type Gender = Omit<Genders, 'id'>
+
+const GENDERS: Gender[] = [
+  {
+    name: 'Mulher CIS',
+    status: 'ativo',
+  },
+  {
+    name: 'Mulher Trans',
+    status: 'ativo',
+  },
+  {
+    name: 'Homem CIS',
+    status: 'ativo',
+  },
+  {
+    name: 'Homem Trans',
+    status: 'ativo',
+  },
+  {
+    name: 'Não-binário',
+    status: 'ativo',
+  },
+  {
+    name: 'Outro',
+    status: 'ativo',
+  },
+]
+
+export async function seedGenders(prisma: PrismaClient) {
+  return Promise.all(
+    await insert({
+      data: GENDERS,
+      name: 'genders',
+      prisma,
+    }),
+  )
+}
