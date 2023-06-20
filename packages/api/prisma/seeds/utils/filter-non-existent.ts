0a8aeaaabae0723394ef0b7aea15a10f657c5481
Ives M. W <ivesmw@gmail.com>
2023-06-19 21:56:44 -0300

    feat: add filterNonExistent seed util

---
+++ packages/api/prisma/seeds/utils/filter-non-existent.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+interface Args {
+  name: string
+  data: unknown[]
+}
+
+export async function filterNonExistent<T>({ data, name }: Args) {
+  const existent = await checkExistent(data, name)
+  const objKeys = Object.keys(data[0])
+
+  const filtered = data.filter(
+    (item) =>
+      !existent.find((existentItem: unknown) => {
+        if (existentItem) {
+          return existentItem[objKeys[0]] === item[objKeys[0]]
+        }
+      }),
+  )
+
+  return filtered as T[]
+}
+
+async function checkExistent(data: unknown[], name: string) {
+  return Promise.all(
+    data.map((item) => {
+      const objKeys = Object.keys(item)
+
+      if (objKeys.length > 1) {
+        const where = objKeys.reduce((acc, key) => {
+          acc[key] = item[key]
+
+          return acc
+        }, {})
+
+        return prisma[name].findFirst({
+          where,
+        })
+      }
+
+      return Promise.resolve(null)
+    }),
+  )
+}
